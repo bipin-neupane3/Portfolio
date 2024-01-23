7aa6939bde7c36d7dd4b51e1a1dff2ed0fb794ac
Bipin Neupane <bipinneupane08@gmail.com>
2024-01-23 10:07:24 +0000

    Personalized images

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Bipin Neupane",
+  title: "Student of Electronics Engineering",
+  email: "bipinneupane005@gmail.com",
+  gitHub: "bipin-neupane3",
+  instagram: "bpn.npne",
+  linkedIn: "https://www.linkedin.com/in/bipinneupane/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
